183461ba8534b8e7df029ac5312a9a5e3aba1387
arturj <arturjardel9@gmail.com>
2023-08-01 09:30:08 -0300

    [add]: Iniciando Command

---
+++ design_patterns_python/src/07_command/cmd01.py
@@ -0,0 +1,27 @@
+class Instalador:
+    def __init__(self, fonte, destino):
+        self.opcoes = []
+        self.destino = destino
+        self.fonte = fonte
+
+    def preferencias(self, escolha):
+        self.opcoes.append(escolha)
+
+    def executar(self):
+        for opcao in self.opcoes:
+            if list(opcao.values())[0]:
+                print(f"Copiando os binários de {self.fonte} para {self.destino}")
+            else:
+                print("Operação finalizada")
+
+
+if __name__ == "__main__":
+    # Inicia o instalador
+    instalador = Instalador("python3.9.1.gzip", "/usr/bin/")
+
+    # O usuário escolhe instalar apenas o Python
+    instalador.preferencias({"python": True})
+    instalador.preferencias({"java": False})
+
+    # Execute
+    instalador.executar()
