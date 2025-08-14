964b973430b558f129d40e87f6abd8833d955b07
arturj9 <arturjardel9@gmail.com>
2025-08-14 10:58:24 -0300

    add slider and switch

--- react-native/meuapp/App.js
+++ react-native/meuapp/App.js
@@ -8,45 +8,31 @@ import {
   TextInput,
   ScrollView,
   FlatList,
+  Switch,
 } from 'react-native';
 
 import Pessoas from './src/Pessoas';
 import { Picker } from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 
-// Picker
-
+// Switch
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pizza: 0,
-      pizzas: [
-        { key: 1, nome: 'Strogonoff', valor: 35.90 },
-        { key: 2, nome: 'Calabresa', valor: 59 },
-        { key: 3, nome: 'Quatro queijos', valor: 37 },
-        { key: 4, nome: 'Brigadeiro', valor: 25.7 },
-        { key: 5, nome: 'Portuguesa', valor: 70 },
-      ],
+      status: false,
     };
   }
 
   render() {
-    let pizzasItem = this.state.pizzas.map((v, k) => {
-      return <Picker.Item key={k} value={k} label={v.nome} />;
-    });
     return (
       <View style={styles.container}>
-        <Text style={styles.logo}>Menu Pizza</Text>
-        <Picker
-          selectedValue={this.state.pizza}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ pizza: itemValue })
-          }
-        >
-          {pizzasItem}
-        </Picker>
-        <Text style={styles.pizzas}>Você escolheu: {this.state.pizzas[this.state.pizza].nome}</Text>
-        <Text style={styles.pizzas}>R$ {this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
+        <Switch
+          value={this.state.status}
+          onValueChange={valorSwitch => this.setState({ status: valorSwitch })}
+          thumbColor='#f700ffff'
+        />
+        <Text style={{textAlign:'center', fontSize:30}}>{(this.state.status) ? 'Ativo': 'Inativo'}</Text>
       </View>
     );
   }
@@ -54,20 +40,97 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: 20,
-  },
-  logo: {
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: 'bold',
-  },
-  pizzas: {
     marginTop: 15,
-    fontSize: 25,
-    textAlign: 'center',
   },
 });
+
+// Slider
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       valor:0
+//     };
+//   }
+
+//   render() {
+//     return <View style={styles.container}>
+//       <Slider
+//       minimumValue={0}
+//       maximumValue={100}
+//       onValueChange={(valorSelecionado) => this.setState({valor:valorSelecionado})}
+//       value={this.state.valor}
+//       minimumTrackTintColor='#00ff00'
+//       maximumTrackTintColor='#ff0000'
+//       thumbTintColor='#008afcff'
+//       />
+//       <Text style={{textAlign:'center', fontSize:30}}>Você tem {this.state.valor.toFixed(0)} kg</Text>
+//     </View>;
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: 15
+//   },
+// });
+
+// Picker
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       pizza: 0,
+//       pizzas: [
+//         { key: 1, nome: 'Strogonoff', valor: 35.90 },
+//         { key: 2, nome: 'Calabresa', valor: 59 },
+//         { key: 3, nome: 'Quatro queijos', valor: 37 },
+//         { key: 4, nome: 'Brigadeiro', valor: 25.7 },
+//         { key: 5, nome: 'Portuguesa', valor: 70 },
+//       ],
+//     };
+//   }
+
+//   render() {
+//     let pizzasItem = this.state.pizzas.map((v, k) => {
+//       return <Picker.Item key={k} value={k} label={v.nome} />;
+//     });
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.logo}>Menu Pizza</Text>
+//         <Picker
+//           selectedValue={this.state.pizza}
+//           onValueChange={(itemValue, itemIndex) =>
+//             this.setState({ pizza: itemValue })
+//           }
+//         >
+//           {pizzasItem}
+//         </Picker>
+//         <Text style={styles.pizzas}>Você escolheu: {this.state.pizzas[this.state.pizza].nome}</Text>
+//         <Text style={styles.pizzas}>R$ {this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: 20,
+//   },
+//   logo: {
+//     textAlign: 'center',
+//     fontSize: 28,
+//     fontWeight: 'bold',
+//   },
+//   pizzas: {
+//     marginTop: 15,
+//     fontSize: 25,
+//     textAlign: 'center',
+//   },
+// });
 
 // FlatList
 // export default class App extends Component {
